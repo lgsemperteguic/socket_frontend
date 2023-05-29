9347ed7d19bf072c278820c4b820498c3c098549
Lenny G Sempertegui C <lenny.sempertegui@uab.edu.bo>
2023-05-28 20:51:48 -0400

    Proyecto Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomComponent } from './room/room.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
