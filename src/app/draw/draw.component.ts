f19539b008bfca171e792dfad4c269cd3c1853c7
Lenny G Sempertegui C <lenny.sempertegui@uab.edu.bo>
2023-05-28 21:30:22 -0400

    Creacion de componentes

---
+++ src/app/draw/draw.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-draw',
+  templateUrl: './draw.component.html',
+  styleUrls: ['./draw.component.css']
+})
+export class DrawComponent {
+
+}
--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.css']
 })
-export class RoomComponent {
+export class RoomComponent implements OnInit{
+  room: any // type any
+  constructor(private router: ActivatedRoute, private cookieService: CookieService){}
 
+  ngOnInit(): void {
+    this.room = this.router.snapshot.paramMap.get('room')
+    this.cookieService.set('room',this.room)
+    console.log(this.room);
+
+  }
 }
